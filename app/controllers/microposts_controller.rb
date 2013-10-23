cba3e42b26c548157a3245085deb060ae35fd930
System Administrator <root@192-168-1-3.tpgi.com.au>
2013-10-23 19:58:56 +1100

    made shit pretty,implemented following via users

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,7 +5,8 @@ class MicropostsController < ApplicationController
     @micropost = current_user.microposts.build(params[:micropost])
     if @micropost.save
       flash[:success] = "Micropost created!"
-      redirect_to root_url
+      redirect_to current_user
+
     else
       @feed_items = []
       render 'static_pages/blogs_home'

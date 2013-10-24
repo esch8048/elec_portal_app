f3623cecac9f371834fdc3d0d38589306b4119bf
mmar3564 <mmar3564@uni.sydney.edu.au>
2013-10-24 19:20:02 +1100

    more fixes to the Forum Page

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,6 +1,8 @@
 class Forum < ActiveRecord::Base
   attr_accessible :name, :description
   has_many :topics, :dependent => :destroy
+  validates_presence_of :name
+  validates_presence_of :description
    
   def most_recent_post  
     topic = Topic.first(:order => 'last_post_at DESC', :conditions => ['forum_id = ?', self.id])  
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,6 @@
 class Topic < ActiveRecord::Base
   attr_accessible :name, :last_poster_id, :last_post_at, :forum_id, :user_id
+  validates_presence_of :name
   belongs_to :forum
   belongs_to :user
   has_many :posts, :dependent => :destroy
